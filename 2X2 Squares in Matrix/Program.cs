951ed8594878ac95f3682c4a7c30a056476f93d3
dackel96 <92174873+dackel96@users.noreply.github.com>
2022-02-19 21:59:04 +0200

    Add files via upload

---
+++ 2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matriX = new char[rowCol[0], rowCol[1]];
+            for (int row = 0; row < matriX.GetLength(0); row++)
+            {
+                char[] rows = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matriX.GetLength(1); col++)
+                {
+                    matriX[row, col] = rows[col];
+                }
+            }
+            int nSquares = 0;
+            for (int row = 0; row < matriX.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matriX.GetLength(1) - 1; col++)
+                {
+                    char currSymbol = matriX[row, col];
+
+                    char topRight = matriX[row, col + 1];
+                    char bottomLeft = matriX[row + 1, col];
+                    char bottomRight = matriX[row + 1, col + 1];
+                    if (currSymbol == topRight && currSymbol == bottomLeft && currSymbol == bottomRight)
+                    {
+                        nSquares++;
+                    }
+                }
+            }
+            Console.WriteLine(nSquares);
+        }
+    }
+}
