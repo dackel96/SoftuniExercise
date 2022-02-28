412e97829b507c2d3611ee49ab28b1b1a0a34ae0
dackel96 <92174873+dackel96@users.noreply.github.com>
2022-02-28 07:40:27 +0200

    Add files via upload

---
+++ NeedForSpeed/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public const double CarFuelConsumption = 3;
+        public Car(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption => CarFuelConsumption;
+    }
+}
---
+++ NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public const double RaceMotorcycleFuelConsumption = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption => RaceMotorcycleFuelConsumption;
+    }
+}
