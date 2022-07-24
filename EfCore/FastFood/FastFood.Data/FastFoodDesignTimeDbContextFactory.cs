49a114d77d1b20ded1753e6fced8868f6486efe6
Ivan Iliev <92174873+dackel96@users.noreply.github.com>
2022-07-24 18:03:36 +0300

    AutoMapperExercise

---
+++ EfCore/FastFood/FastFood.Data/FastFoodDesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    class FastFoodDesignTimeDbContextFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            //TODO Hide connection string
+            builder.UseSqlServer(@"Server=DESKTOP-73BH94J\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
